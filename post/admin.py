aa3a98eedd117822f14269fac17b9ae9cf97d89f
islashik <islamrahatbekov499@gmail.com>
2022-03-24 20:53:56 +0600

    initial commit

---
+++ post/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post, Comment, Category
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'created', 'is_active']
+    list_filter = ['created','author','category']
+    search_fields = ['title','id', 'text','created']
+    list_editable = ['category', 'is_active']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'id']
+    prepopulated_fields = {'slug':('name',)}    
---
+++ post/views.py
@@ -0,0 +1,15 @@
+from http.client import HTTPResponse
+from unicodedata import category
+from django.shortcuts import render
+from django.http import HttpResponse
+from post.models import Post, Category
+
+
+def index(request):
+    posts = Post.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'posts':posts,
+        'categories':categories
+    }
+    return render(request, 'index.html', context = context)
